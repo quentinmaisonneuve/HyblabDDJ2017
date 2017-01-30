1b37614c0fbde8c284636637800f17f019818ebc
Aurelie Durand <aurelie.durand1@etu.univ-nantes.fr>
2017-01-30 22:04:27 +0100

    modif graphe 2013, rangement dossiers

--- double_mixte/public/js/2013-chart.js
+++ double_mixte/public/js/2013-chart.js
@@ -1,33 +1,67 @@
-$(function(){
-    $("#chart").dxChart({
-        dataSource: dataSource,
-        commonSeriesSettings: {
-            argumentField: "annee",
-            type: "bar",
-            hoverMode: "allArgumentPoints",
-            selectionMode: "allArgumentPoints",
-            label: {
-                visible: true,
-                format: {
-                    type: "fixedPoint",
-                    precision: 0
+document.addEventListener('DOMContentLoaded',function(){
+    var data = {
+        labels: ['2011', '2012', '2013'],
+        series: [
+            [60356.99009, 59817.68384, 72346.9775],
+            [72929.96518, 67039.10476, 69870.0879]
+        ]
+    };
+
+    var options = {
+        seriesBarDistance: 20,
+        height: "20em"
+    };
+
+    var responsiveOptions = [
+        ['screen and (min-width: 641px) and (max-width: 1024px)', {
+            seriesBarDistance: 15,
+            axisX: {
+                labelInterpolationFnc: function (value) {
+                    return value;
+            }
+        }
+        }],
+        ['screen and (max-width: 640px)', {
+            seriesBarDistance: 13,
+            showLine: false,
+            height: "10em",
+            axisX: {
+                labelInterpolationFnc: function (value) {
+                    return value[0];
                 }
             }
-        },
-        series: [
-            { valueField: "aout", name: "Août" },
-            { valueField: "octobre", name: "Octobre" }
-        ],
-        title: "Nombre de nuitées",
-        legend: {
-            verticalAlignment: "bottom",
-            horizontalAlignment: "center"
-        },
-        "export": {
-            enabled: false
-        },
-        onPointClick: function (e) {
-            e.target.select();
+        }]
+    ];
+
+new Chartist.Bar('.ct-chart', data, options, responsiveOptions);
+
+$('#chart1').on('draw', function (data) {
+    if (data.type === 'bar') {
+        data.element.attr({
+            style: 'stroke-width: 0px'
+        });
+        var strokeWidth = 10;
+
+        for (var s = 0; s < data.series.length; ++s) {
+            if (data.seriesIndex === s) {
+                data.element.animate({
+                    y2:             {
+                        begin:  s * 500,
+                        dur:    500,
+                        from:   data.y1,
+                        to:     data.y2,
+                        easing: Chartist.Svg.Easing.easeOutSine
+                    },
+                    'stroke-width': {
+                        begin: s * 500,
+                        dur:   1,
+                        from:  0,
+                        to:    strokeWidth,
+                        fill:  'freeze'
+                    }
+                }, false);
+            }
         }
-    });
+    }
+});
 });
---
+++ double_mixte/public/js/slider.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+		sectionsColor: ['#ffffff'],
+		anchors: ['firstPage'],
+		menu: '#menu',
+		scrollingSpeed: 1000
+	});
+});
