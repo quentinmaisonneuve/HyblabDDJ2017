1b85df74df9d03ebae91d60ca94b3f7c84bae5ce
Raphael Barraud <raphael.barraud@hotmail.fr>
2017-02-02 14:30:58 +0100

    getMood

--- sun/API/data.js
+++ sun/API/data.js
@@ -149,6 +149,194 @@ var data = {
             return db.query("SELECT count(*) AS value, _genre4 AS id FROM winmedia_media W LEFT JOIN musique_auditeur_tbl M ON M.id_musique = W._jazler_id WHERE "+intervalle+" AND WEEKDAY(date_heure_diffusion_reelle)>='5' AND HOUR(date_heure_diffusion_reelle)>='"+start+"' AND HOUR(date_heure_diffusion_reelle)<='"+end+"' GROUP BY _genre4;", callback)
         //return db.query("SELECT count(*), _genre4 FROM winmedia_media W LEFT JOIN musique_auditeur_tbl M ON M.id_musique = W._jazler_id WHERE DAYOFYEAR(date_heure_diffusion_reelle) < DAYOFYEAR(NOW()) AND DAYOFYEAR(date_heure_diffusion_reelle) >= (DAYOFYEAR(NOW())-1) GROUP BY _genre4;", callback)
     }
+
+
+//Sentimentale
+
+    getSentimentaleSeasonWeekHour:function(season, week, start, end, callback)
+    {
+        var intervalle = "";
+        switch(season)
+        {
+            case "winter":
+                intervalle = "DAYOFYEAR(date_heure_diffusion_reelle) < 80 OR DAYOFYEAR(date_heure_diffusion_reelle) >= 355";
+                break;
+            case "spring":
+                intervalle = "DAYOFYEAR(date_heure_diffusion_reelle) >= 80 AND DAYOFYEAR(date_heure_diffusion_reelle) < 172";
+                break;
+            case "summer":
+                intervalle = "DAYOFYEAR(date_heure_diffusion_reelle) >= 172 AND DAYOFYEAR(date_heure_diffusion_reelle) < 264";
+                break;
+            case "autumn":
+                intervalle = "DAYOFYEAR(date_heure_diffusion_reelle) >= 264 AND DAYOFYEAR(date_heure_diffusion_reelle) < 355";
+                break;
+            default:
+                throw "Bad season";
+                break;
+        }
+
+        if ( (!/\d/.test(week)) &&  (week.length!=1) )
+            throw "Bad week format";
+        else if (intervalle == "")
+            throw "Bad interval format";
+        else if(season > 6 && !/\D{6}/.test(season))
+            throw "Bad season format";
+        else
+        if (week == 0) // Week
+            return db.query("SELECT count(*) AS value, _genre4 AS id FROM winmedia_media W LEFT JOIN musique_auditeur_tbl M ON M.id_musique = W._jazler_id WHERE "+intervalle+" AND WEEKDAY(date_heure_diffusion_reelle)<='4' AND HOUR(date_heure_diffusion_reelle)>='"+start+"' AND HOUR(date_heure_diffusion_reelle)<='"+end+"' AND (_mood1="Sentimentale" OR _mood1="Romantique" OR _mood1="Réaliste" OR _mood1="Sophistiquée"  OR _mood1="Sérieuse" OR _mood1="Tendre");", callback)
+        else if (week == 1) // Week End
+            return db.query("SELECT count(*) AS value, _genre4 AS id FROM winmedia_media W LEFT JOIN musique_auditeur_tbl M ON M.id_musique = W._jazler_id WHERE "+intervalle+" AND WEEKDAY(date_heure_diffusion_reelle)>='5' AND HOUR(date_heure_diffusion_reelle)>='"+start+"' AND HOUR(date_heure_diffusion_reelle)<='"+end+"' AND (_mood1="Sentimentale" OR _mood1="Romantique" OR _mood1="Réaliste" OR _mood1="Sophistiquée"  OR _mood1="Sérieuse" OR _mood1="Tendre");", callback)
+    }
+
+
+//Agressive
+
+    getAgressiveSeasonWeekHour:function(season, week, start, end, callback)
+    {
+        var intervalle = "";
+        switch(season)
+        {
+            case "winter":
+                intervalle = "DAYOFYEAR(date_heure_diffusion_reelle) < 80 OR DAYOFYEAR(date_heure_diffusion_reelle) >= 355";
+                break;
+            case "spring":
+                intervalle = "DAYOFYEAR(date_heure_diffusion_reelle) >= 80 AND DAYOFYEAR(date_heure_diffusion_reelle) < 172";
+                break;
+            case "summer":
+                intervalle = "DAYOFYEAR(date_heure_diffusion_reelle) >= 172 AND DAYOFYEAR(date_heure_diffusion_reelle) < 264";
+                break;
+            case "autumn":
+                intervalle = "DAYOFYEAR(date_heure_diffusion_reelle) >= 264 AND DAYOFYEAR(date_heure_diffusion_reelle) < 355";
+                break;
+            default:
+                throw "Bad season";
+                break;
+        }
+
+        if ( (!/\d/.test(week)) &&  (week.length!=1) )
+            throw "Bad week format";
+        else if (intervalle == "")
+            throw "Bad interval format";
+        else if(season > 6 && !/\D{6}/.test(season))
+            throw "Bad season format";
+        else
+        if (week == 0) // Week
+            return db.query("SELECT count(*) AS value, _genre4 AS id FROM winmedia_media W LEFT JOIN musique_auditeur_tbl M ON M.id_musique = W._jazler_id WHERE "+intervalle+" AND WEEKDAY(date_heure_diffusion_reelle)<='4' AND HOUR(date_heure_diffusion_reelle)>='"+start+"' AND HOUR(date_heure_diffusion_reelle)<='"+end+"'AND (_mood1="Chahuteuse" OR _mood1="Provocante" OR _mood1="Agressive" OR _mood1="Ténébreuse");", callback)
+        else if (week == 1) // Week End
+            return db.query("SELECT count(*) AS value, _genre4 AS id FROM winmedia_media W LEFT JOIN musique_auditeur_tbl M ON M.id_musique = W._jazler_id WHERE "+intervalle+" AND WEEKDAY(date_heure_diffusion_reelle)>='5' AND HOUR(date_heure_diffusion_reelle)>='"+start+"' AND HOUR(date_heure_diffusion_reelle)<='"+end+"'AND (_mood1="Chahuteuse" OR _mood1="Provocante" OR _mood1="Agressive" OR _mood1="Ténébreuse");", callback)
+    }
+
+
+//Stimulante
+
+    getStimulanteSeasonWeekHour:function(season, week, start, end, callback)
+    {
+        var intervalle = "";
+        switch(season)
+        {
+            case "winter":
+                intervalle = "DAYOFYEAR(date_heure_diffusion_reelle) < 80 OR DAYOFYEAR(date_heure_diffusion_reelle) >= 355";
+                break;
+            case "spring":
+                intervalle = "DAYOFYEAR(date_heure_diffusion_reelle) >= 80 AND DAYOFYEAR(date_heure_diffusion_reelle) < 172";
+                break;
+            case "summer":
+                intervalle = "DAYOFYEAR(date_heure_diffusion_reelle) >= 172 AND DAYOFYEAR(date_heure_diffusion_reelle) < 264";
+                break;
+            case "autumn":
+                intervalle = "DAYOFYEAR(date_heure_diffusion_reelle) >= 264 AND DAYOFYEAR(date_heure_diffusion_reelle) < 355";
+                break;
+            default:
+                throw "Bad season";
+                break;
+        }
+
+        if ( (!/\d/.test(week)) &&  (week.length!=1) )
+            throw "Bad week format";
+        else if (intervalle == "")
+            throw "Bad interval format";
+        else if(season > 6 && !/\D{6}/.test(season))
+            throw "Bad season format";
+        else
+        if (week == 0) // Week
+            return db.query("SELECT count(*) AS value, _genre4 AS id FROM winmedia_media W LEFT JOIN musique_auditeur_tbl M ON M.id_musique = W._jazler_id WHERE "+intervalle+" AND WEEKDAY(date_heure_diffusion_reelle)<='4' AND HOUR(date_heure_diffusion_reelle)>='"+start+"' AND HOUR(date_heure_diffusion_reelle)<='"+end+"'AND (_mood1="Stimulante" OR _mood1="Vigoureuse" OR _mood1="Exaltante" OR _mood1="Sensuelle" OR _mood1="Enjouée" OR _mood1="Agitée" );", callback)
+        else if (week == 1) // Week End
+            return db.query("SELECT count(*) AS value, _genre4 AS id FROM winmedia_media W LEFT JOIN musique_auditeur_tbl M ON M.id_musique = W._jazler_id WHERE "+intervalle+" AND WEEKDAY(date_heure_diffusion_reelle)>='5' AND HOUR(date_heure_diffusion_reelle)>='"+start+"' AND HOUR(date_heure_diffusion_reelle)<='"+end+"'AND (_mood1="Stimulante" OR _mood1="Vigoureuse" OR _mood1="Exaltante" OR _mood1="Sensuelle" OR _mood1="Enjouée" OR _mood1="Agitée");", callback)
+    }
+};
+
+//cool
+    getCoolSeasonWeekHour:function(season, week, start, end, callback)
+    {
+        var intervalle = "";
+        switch(season)
+        {
+            case "winter":
+                intervalle = "DAYOFYEAR(date_heure_diffusion_reelle) < 80 OR DAYOFYEAR(date_heure_diffusion_reelle) >= 355";
+                break;
+            case "spring":
+                intervalle = "DAYOFYEAR(date_heure_diffusion_reelle) >= 80 AND DAYOFYEAR(date_heure_diffusion_reelle) < 172";
+                break;
+            case "summer":
+                intervalle = "DAYOFYEAR(date_heure_diffusion_reelle) >= 172 AND DAYOFYEAR(date_heure_diffusion_reelle) < 264";
+                break;
+            case "autumn":
+                intervalle = "DAYOFYEAR(date_heure_diffusion_reelle) >= 264 AND DAYOFYEAR(date_heure_diffusion_reelle) < 355";
+                break;
+            default:
+                throw "Bad season";
+                break;
+        }
+
+        if ( (!/\d/.test(week)) &&  (week.length!=1) )
+            throw "Bad week format";
+        else if (intervalle == "")
+            throw "Bad interval format";
+        else if(season > 6 && !/\D{6}/.test(season))
+            throw "Bad season format";
+        else
+        if (week == 0) // Week
+            return db.query("SELECT count(*) AS value, _genre4 AS id FROM winmedia_media W LEFT JOIN musique_auditeur_tbl M ON M.id_musique = W._jazler_id WHERE "+intervalle+" AND WEEKDAY(date_heure_diffusion_reelle)<='4' AND HOUR(date_heure_diffusion_reelle)>='"+start+"' AND HOUR(date_heure_diffusion_reelle)<='"+end+"'AND (_mood1="Brûlante" OR _mood1="Cool" OR _mood1="Enflammée" OR _mood1="Insouciante" OR _mood1="Paisible");", callback)
+        else if (week == 1) // Week End
+            return db.query("SELECT count(*) AS value, _genre4 AS id FROM winmedia_media W LEFT JOIN musique_auditeur_tbl M ON M.id_musique = W._jazler_id WHERE "+intervalle+" AND WEEKDAY(date_heure_diffusion_reelle)>='5' AND HOUR(date_heure_diffusion_reelle)>='"+start+"' AND HOUR(date_heure_diffusion_reelle)<='"+end+"'AND (_mood1="Brûlante" OR _mood1="Cool" OR _mood1="Enflammée" OR _mood1="Insouciante" OR _mood1="Paisible");", callback)
+    }
+};
+
+//Nostalgique
+    getNostalgiqueSeasonWeekHour:function(season, week, start, end, callback)
+    {
+        var intervalle = "";
+        switch(season)
+        {
+            case "winter":
+                intervalle = "DAYOFYEAR(date_heure_diffusion_reelle) < 80 OR DAYOFYEAR(date_heure_diffusion_reelle) >= 355";
+                break;
+            case "spring":
+                intervalle = "DAYOFYEAR(date_heure_diffusion_reelle) >= 80 AND DAYOFYEAR(date_heure_diffusion_reelle) < 172";
+                break;
+            case "summer":
+                intervalle = "DAYOFYEAR(date_heure_diffusion_reelle) >= 172 AND DAYOFYEAR(date_heure_diffusion_reelle) < 264";
+                break;
+            case "autumn":
+                intervalle = "DAYOFYEAR(date_heure_diffusion_reelle) >= 264 AND DAYOFYEAR(date_heure_diffusion_reelle) < 355";
+                break;
+            default:
+                throw "Bad season";
+                break;
+        }
+
+        if ( (!/\d/.test(week)) &&  (week.length!=1) )
+            throw "Bad week format";
+        else if (intervalle == "")
+            throw "Bad interval format";
+        else if(season > 6 && !/\D{6}/.test(season))
+            throw "Bad season format";
+        else
+        if (week == 0) // Week
+            return db.query("SELECT count(*) AS value, _genre4 AS id FROM winmedia_media W LEFT JOIN musique_auditeur_tbl M ON M.id_musique = W._jazler_id WHERE "+intervalle+" AND WEEKDAY(date_heure_diffusion_reelle)<='4' AND HOUR(date_heure_diffusion_reelle)>='"+start+"' AND HOUR(date_heure_diffusion_reelle)<='"+end+"'AND (_mood1="Grisante" OR _mood1="Mélancolique" OR _mood1="Nostalgique" OR _mood1="Tourmentée");", callback)
+        else if (week == 1) // Week End
+            return db.query("SELECT count(*) AS value, _genre4 AS id FROM winmedia_media W LEFT JOIN musique_auditeur_tbl M ON M.id_musique = W._jazler_id WHERE "+intervalle+" AND WEEKDAY(date_heure_diffusion_reelle)>='5' AND HOUR(date_heure_diffusion_reelle)>='"+start+"' AND HOUR(date_heure_diffusion_reelle)<='"+end+"'AND (_mood1="Grisante" OR _mood1="Mélancolique" OR _mood1="Nostalgique" OR _mood1="Tourmentée");", callback)
+    }
 };
 
 // Function verify if a date format sql is correct
