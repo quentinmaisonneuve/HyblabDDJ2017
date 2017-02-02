7e21989e3dd7e8b35a65fd8a162f45bdfcfad160
Aurelie Durand <aurelie.durand1@etu.univ-nantes.fr>
2017-02-02 20:07:14 +0100

    ajout digits 2012

--- double_mixte/public/js/2012.js
+++ double_mixte/public/js/2012.js
@@ -1,63 +1,63 @@
-function animateDigits2013() {
+function animateDigits2012() {
 	$.get("data/nuiteesannuelles.json", function(data) {
-    	var nbNuiteesPrecedentes = data[6].nbnuitees;
-        var nbNuitees = data[7].nbnuitees;
+    	var nbNuiteesPrecedentes = data[5].nbnuitees;
+        var nbNuitees = data[6].nbnuitees;
         var array = nbNuitees.toString().split("").map(function(t){return parseInt(t)});
         var array2 = nbNuiteesPrecedentes.toString().split("").map(function(t){return parseInt(t)});
         
-        replaceAllDigits(array2, 2013);
+        replaceAllDigits(array2, 2012);
        
-        setTimeout(function(){ $('#2013chiffre0').animateCss20130('flipInX'); }, 1000);
-	    setTimeout(function(){ $('#2013chiffre1').animateCss20131('flipInX'); }, 1100);
-	    setTimeout(function(){ $('#2013chiffre2').animateCss20132('flipInX');}, 1200);
-	    setTimeout(function(){ $('#2013chiffre3').animateCss20133('flipInX');}, 1300);
-	    setTimeout(function(){ $('#2013chiffre4').animateCss20134('flipInX');}, 1400);
-	    setTimeout(function(){ $('#2013chiffre5').animateCss20135('flipInX');}, 1500);
-	    setTimeout(function(){ $('#2013chiffre6').animateCss20136('flipInX');}, 1600);
+        setTimeout(function(){ $('#2012chiffre0').animateCss20120('flipInX'); }, 1000);
+	    setTimeout(function(){ $('#2012chiffre1').animateCss20121('flipInX'); }, 1100);
+	    setTimeout(function(){ $('#2012chiffre2').animateCss20122('flipInX');}, 1200);
+	    setTimeout(function(){ $('#2012chiffre3').animateCss20123('flipInX');}, 1300);
+	    setTimeout(function(){ $('#2012chiffre4').animateCss20124('flipInX');}, 1400);
+	    setTimeout(function(){ $('#2012chiffre5').animateCss20125('flipInX');}, 1500);
+	    setTimeout(function(){ $('#2012chiffre6').animateCss20126('flipInX');}, 1600);
         
-	    $('.2013chiffre').removeClass('animated flipInX');
+	    $('.2012chiffre').removeClass('animated flipInX');
 
 	    $.fn.extend({
-		    animateCss20130: function (animationName) {
+		    animateCss20120: function (animationName) {
 		        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
 		        this.addClass('animated ' + animationName).one(animationEnd, function() {
-	            	replaceDigit(0, array, 2013); 
+	            	replaceDigit(0, array, 2012); 
 		        });
     		},
-    		animateCss20131: function (animationName) {
+    		animateCss20121: function (animationName) {
 		        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
 		        this.addClass('animated ' + animationName).one(animationEnd, function() {    
-	            	replaceDigit(1, array, 2013);
+	            	replaceDigit(1, array, 2012);
 		        });
     		},
-    		animateCss20132: function (animationName) {
+    		animateCss20122: function (animationName) {
 		        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
 		        this.addClass('animated ' + animationName).one(animationEnd, function() {    
-	            	replaceDigit(2, array, 2013);
+	            	replaceDigit(2, array, 2012);
 		        });
     		},
-    		animateCss20133: function (animationName) {
+    		animateCss20123: function (animationName) {
 		        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
 		        this.addClass('animated ' + animationName).one(animationEnd, function() {
-	            	replaceDigit(3, array, 2013); 
+	            	replaceDigit(3, array, 2012); 
 		        });
     		},
-    		animateCss20134: function (animationName) {
+    		animateCss20124: function (animationName) {
 		        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
 		        this.addClass('animated ' + animationName).one(animationEnd, function() {
-	            	replaceDigit(4, array, 2013);
+	            	replaceDigit(4, array, 2012);
 		        });
     		},
-    		animateCss20135: function (animationName) {
+    		animateCss20125: function (animationName) {
 		        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
 		        this.addClass('animated ' + animationName).one(animationEnd, function() {
-	            	replaceDigit(5, array, 2013);  
+	            	replaceDigit(5, array, 2012);  
 		        });
     		},
-    		animateCss20136: function (animationName) {
+    		animateCss20126: function (animationName) {
 		        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
 		        this.addClass('animated ' + animationName).one(animationEnd, function() {
-	            	replaceDigit(6, array, 2013);
+	            	replaceDigit(6, array, 2012);
 		        });
     		}
 		});
