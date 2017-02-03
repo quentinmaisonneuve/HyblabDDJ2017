d2c31ada3ac235bbc4df77c0704c42cee5ff3c05
leesiyuan <leesiyuan@qq.com>
2017-02-03 16:18:15 +0100

    Add Twitter, delete small labels

--- euradio_nantes/public/js/data.js
+++ euradio_nantes/public/js/data.js
@@ -45,7 +45,7 @@ function creatFacebook(){
                 toolTipContent: "<span style='\"'color: {color};'\"'><strong>{indexLabel}</strong></span><span style='\"'font-size: 20px; color:peru '\"'><strong>{y}</strong></span>",
 
                 indexLabelPlacement: "inside",
-                indexLabelFontColor: "#FF6666",
+                indexLabelFontColor: "red",
                 indexLabelFontWeight: 600,
                 indexLabelFontFamily: "Verdana",
                 color: "#3b5998",
@@ -100,12 +100,12 @@ function creatTwitter(){
                 toolTipContent: "<span style='\"'color: {color};'\"'><strong>{indexLabel}</strong></span><span style='\"'font-size: 20px; color:peru '\"'><strong>{y}</strong></span>",
 
                 indexLabelPlacement: "",
-                indexLabelFontColor: "red",
+                indexLabelFontColor: "#FF6666",
                 indexLabelFontWeight: 600,
                 indexLabelFontFamily: "Verdana",
                 color: "#00acee",
                 type: "bar",
-                dataPoints: provenancePaysPourcentageFacebook.reverse()
+                dataPoints: provenancePaysPourcentageTwitter.reverse()
                 /*[
                     { y: 21, label: "21%", indexLabel: "Suisse" },
                     { y: 25, label: "25%", indexLabel: "Pays-pas" },
@@ -138,11 +138,11 @@ function creatTwitter(){
 var provenancePaysPourcentageFacebook = 
 [
 
-      /*{
+      {
         "label": "France",
         "y": 6352,
         "indexLabel": "81,59%"
-      },*/
+      },
       {
         "label": "Italie",
         "y": 206,
@@ -170,7 +170,7 @@ var provenancePaysPourcentageFacebook =
         "indexLabel": "1,07%"
       },
       {
-        "label": "Etats Unis",
+        "label": "Etats Unis%",
         "y": 63,
         "indexLabel": "0,81%"
       },
@@ -188,17 +188,12 @@ var provenancePaysPourcentageFacebook =
         "label": "Portugal",
         "y": 31,
         "indexLabel": "0,4%"
-      }/*,
-      {
-        "label": "Autre",
-        "y": 386,
-        "indexLabel": "6.99%"
-      }*/,
+      },
       {
         "label": "Algérie",
         "y": 29,
         "indexLabel": "0,37%"
-      }/*,
+      },
       {
         "label": "Grèce",
         "y": 29,
@@ -249,7 +244,7 @@ var provenancePaysPourcentageFacebook =
         "label": "Hongrie",
         "y": 13,
         "indexLabel": "0,17%"
-      },
+      }/*,
       {
         "label": "Bulgarie",
         "y": 13,
@@ -371,3 +366,55 @@ var provenancePaysPourcentageFacebook =
         "indexLabel": "0,06"
       }*/
     ]
+var provenancePaysPourcentageTwitter =[
+    /*{
+      "y": "79"
+      "label": "France",
+      "indexLabel": "79"
+    },*/
+    {
+      "y": 6,
+      "label": "Belgique",
+      "indexLabel": "6%"
+    },
+    {
+      "y": 3,
+      "label": "Royaume-Uni",
+      "indexLabel": "3%"
+    },
+    {
+      "y": 2,
+      "label": "Italie",
+      "indexLabel": "2%"
+    },
+    {
+      "y": 1,
+      "label": "Espagne",
+      "indexLabel": "1%"
+    },
+    {
+      "y": 1,
+      "label": "Allemagne",
+      "indexLabel": "1%"
+    },
+    {
+      "y": 1,
+      "label": "Etats-Unis",
+      "indexLabel": "1%"
+    },
+    {
+      "y": 1,
+      "label": "Pays-Bas",
+      "indexLabel": "1%"
+    },
+    {
+      "y": 1,
+      "label": "Canada",
+      "indexLabel": "1%"
+    },
+    {
+      "y": 1,
+      "label": "Grèce",
+      "indexLabel": "1%"
+    }
+  ]
