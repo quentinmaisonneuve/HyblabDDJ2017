41b171fb76148412636d894fc072d768e8f365ed
Aurelie Durand <aurelie.durand1@etu.univ-nantes.fr>
2017-02-04 19:47:54 +0100

    animation presse 2013

--- double_mixte/public/js/popover.js
+++ double_mixte/public/js/popover.js
@@ -5,3 +5,11 @@ function showPopover2013() {
 function hidePopover2013() {
 	$("#popover2013").css("display", "none");
 }
+
+function showPresse2013() {
+	setTimeout(function(){ $("#presse2013").css("display", "block"); }, 1000);
+}
+
+function hidePresse2013() {
+	$("#presse2013").css("display", "none");
+}
