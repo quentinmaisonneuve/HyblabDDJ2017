515f5efee55ad91df421d71459941f9b5d5708fe
Aurelie Durand <aurelie.durand1@etu.univ-nantes.fr>
2017-02-04 18:25:18 +0100

    animation texte 2013

--- double_mixte/public/js/popover.js
+++ double_mixte/public/js/popover.js
@@ -1,5 +1,7 @@
 function showPopover2013() {
-
-	
 	setTimeout(function(){ $("#popover2013").css("display", "block"); }, 1000);
+}
+
+function hidePopover2013() {
+	$("#popover2013").css("display", "none");
 }
