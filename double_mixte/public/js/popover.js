63c037d7e4568252ac6d6442dd0089609bfb1164
Aurelie Durand <aurelie.durand1@etu.univ-nantes.fr>
2017-02-04 23:13:31 +0100

    fun fact 2013 animation

--- double_mixte/public/js/popover.js
+++ double_mixte/public/js/popover.js
@@ -1,9 +1,13 @@
 function showPopover2013() {
-	setTimeout(function(){ $("#popover2013").css("display", "block"); }, 1000);
+	setTimeout(function(){ $("#popover2013").css("display", "block"); }, 2000);
+	setTimeout(function(){ $("#textehistoire2013").css("display", "block"); }, 1000);
+	setTimeout(function(){ $("#funfact2013").css("display", "block"); }, 1000);
 }
 
 function hidePopover2013() {
 	$("#popover2013").css("display", "none");
+	$("#textehistoire2013").css("display", "none");
+	$("#funfact2013").css("display", "none");
 }
 
 function showPresse2013() {
@@ -13,3 +17,4 @@ function showPresse2013() {
 function hidePresse2013() {
 	$("#presse2013").css("display", "none");
 }
+
