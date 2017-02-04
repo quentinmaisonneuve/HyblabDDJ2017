06fcb12f0e46569a15b1be36ea350dbc2d35f5b2
Aurelie Durand <aurelie.durand1@etu.univ-nantes.fr>
2017-02-04 12:53:42 +0100

    ajout social 2012

--- double_mixte/public/js/social.js
+++ double_mixte/public/js/social.js
@@ -1,4 +1,104 @@
 $.fn.extend({
+    animateFacebook20120: function (animationName, array, annee, label) {
+        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+        this.addClass('animated ' + animationName).one(animationEnd, function() {
+            replaceDigitSocial(0, array, annee, label); 
+        });
+    },
+	animateFacebook20121: function (animationName, array, annee, label) {
+        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+        this.addClass('animated ' + animationName).one(animationEnd, function() {
+            replaceDigitSocial(1, array, annee, label); 
+        });
+    },
+	animateFacebook20122: function (animationName, array, annee, label) {
+	    var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+	    this.addClass('animated ' + animationName).one(animationEnd, function() {
+	        replaceDigitSocial(2, array, annee, label); 
+    	});
+    },
+    animateFacebook20123: function (animationName, array, annee, label) {
+	    var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+	    this.addClass('animated ' + animationName).one(animationEnd, function() {
+	        replaceDigitSocial(3, array, annee, label); 
+    	});
+    },
+    animateFacebook20124: function (animationName, array, annee, label) {
+	    var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+	    this.addClass('animated ' + animationName).one(animationEnd, function() {
+	        replaceDigitSocial(4, array, annee, label); 
+    	});
+    },
+    animateTwitter20120: function (animationName, array, annee, label) {
+	    var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+	    this.addClass('animated ' + animationName).one(animationEnd, function() {
+	        replaceDigitSocial(0, array, annee, label); 
+    	});
+    },
+    animateTwitter20121: function (animationName, array, annee, label) {
+	    var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+	    this.addClass('animated ' + animationName).one(animationEnd, function() {
+	        replaceDigitSocial(1, array, annee, label); 
+    	});
+    },
+    animateTwitter20122: function (animationName, array, annee, label) {
+	    var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+	    this.addClass('animated ' + animationName).one(animationEnd, function() {
+	        replaceDigitSocial(2, array, annee, label); 
+    	});
+    },
+    animateTwitter20123: function (animationName, array, annee, label) {
+	    var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+	    this.addClass('animated ' + animationName).one(animationEnd, function() {
+	        replaceDigitSocial(3, array, annee, label); 
+    	});
+    },
+    animateTwitter20124: function (animationName, array, annee, label) {
+	    var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+	    this.addClass('animated ' + animationName).one(animationEnd, function() {
+	        replaceDigitSocial(4, array, annee, label); 
+    	});
+    },
+    animateInstagram20120: function (animationName, array, annee, label) {
+	    var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+	    this.addClass('animated ' + animationName).one(animationEnd, function() {
+	        replaceDigitSocial(0, array, annee, label); 
+    	});
+    },
+    animateInstagram20121: function (animationName, array, annee, label) {
+	    var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+	    this.addClass('animated ' + animationName).one(animationEnd, function() {
+	        replaceDigitSocial(1, array, annee, label); 
+    	});
+    },
+    animateInstagram20122: function (animationName, array, annee, label) {
+	    var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+	    this.addClass('animated ' + animationName).one(animationEnd, function() {
+	        replaceDigitSocial(2, array, annee, label); 
+    	});
+    },
+    animateInstagram20123: function (animationName, array, annee, label) {
+	    var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+	    this.addClass('animated ' + animationName).one(animationEnd, function() {
+	        replaceDigitSocial(3, array, annee, label); 
+    	});
+    },
+    animateInstagram20124: function (animationName, array, annee, label) {
+	    var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+	    this.addClass('animated ' + animationName).one(animationEnd, function() {
+	        replaceDigitSocial(4, array, annee, label); 
+    	});
+    },
+
+
+
+
+
+
+
+
+
+
     animateFacebook20130: function (animationName, array, annee, label) {
         var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
         this.addClass('animated ' + animationName).one(animationEnd, function() {
@@ -58,6 +158,36 @@ $.fn.extend({
 	    this.addClass('animated ' + animationName).one(animationEnd, function() {
 	        replaceDigitSocial(4, array, annee, label); 
     	});
+    },
+    animateInstagram20130: function (animationName, array, annee, label) {
+	    var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+	    this.addClass('animated ' + animationName).one(animationEnd, function() {
+	        replaceDigitSocial(0, array, annee, label); 
+    	});
+    },
+    animateInstagram20131: function (animationName, array, annee, label) {
+	    var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+	    this.addClass('animated ' + animationName).one(animationEnd, function() {
+	        replaceDigitSocial(1, array, annee, label); 
+    	});
+    },
+    animateInstagram20132: function (animationName, array, annee, label) {
+	    var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+	    this.addClass('animated ' + animationName).one(animationEnd, function() {
+	        replaceDigitSocial(2, array, annee, label); 
+    	});
+    },
+    animateInstagram20133: function (animationName, array, annee, label) {
+	    var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+	    this.addClass('animated ' + animationName).one(animationEnd, function() {
+	        replaceDigitSocial(3, array, annee, label); 
+    	});
+    },
+    animateInstagram20134: function (animationName, array, annee, label) {
+	    var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+	    this.addClass('animated ' + animationName).one(animationEnd, function() {
+	        replaceDigitSocial(4, array, annee, label); 
+    	});
     }
 });
 
@@ -97,9 +227,9 @@ function replaceDigitSocial(digit, array, annee, label) {
         default:
         	image = 'blanc.svg';
     }
-    if(image != 'blanc.svg') {
-    	$('#'+annee+label+digit).attr('src', 'img/'+image);
-    }
+
+    $('#'+annee+label+digit).attr('src', 'img/'+image);
+    
     
 }
 
@@ -128,6 +258,12 @@ function animateSocialFacebook(annee) {
 		indice = 4;
 	}
 
+	 if(annee === 2012) {
+        $('.2012insta').attr('src', 'img/blanc.svg');
+        $('.2012facebook').attr('src', 'img/blanc.svg');
+        $('.2012twitter').attr('src', 'img/blanc.svg');
+    }
+
 	$.get("data/data-facebook.json", function(data) {
 		if(annee != 2012) {
 			var nbFacebookPrecedents = data[indice-1].nombre;
@@ -146,9 +282,10 @@ function animateSocialFacebook(annee) {
 			}
 			replaceAllDigitsSocial(array2, annee, "facebook");
 		}
-		
-		if((5-array.length)!=0) {
-			for(var i=0; i<=(5-array.length); i++) {
+		var l = array.length;
+		var e = 5-l;
+		if((5-l) != 0) {
+			for(var i=0; i<e; i++) {
 				array.unshift(-1);
 			}
 		}
@@ -190,7 +327,7 @@ function animateSocialTwitter(annee) {
 		var array = nbTwitter.toString().split("").map(function(t){return parseInt(t)});
 
 		var label = "twitter";
-		
+
 		// données de l'année précédente
 		if(annee != 2012) {
 			if((5-array2.length)!=0) {
@@ -201,8 +338,10 @@ function animateSocialTwitter(annee) {
 			replaceAllDigitsSocial(array2, annee, label);
 		}
 		
-		if((5-array.length)!=0) {
-			for(var i=0; i<=(5-array.length); i++) {
+		var l = array.length;
+		var e = 5-l;
+		if((5-l) != 0) {
+			for(var i=0; i<e; i++) {
 				array.unshift(-1);
 			}
 		}
@@ -214,5 +353,64 @@ function animateSocialTwitter(annee) {
         setTimeout(function(){ $('#'+annee+'twitter4')['animateTwitter'+annee+4]('flipInX', array, annee, label);}, 1400);
 
        	$('.'+annee+'twitter').removeClass('animated flipInX')
+	});
+}
+
+function animateSocialInstagram(annee) {
+	var indice;
+	if(annee === 2012) {
+		indice = 0;
+	}
+	else if(annee == 2013) {
+		indice = 1;
+	}
+	else if(annee === 2014) {
+		indice = 2;
+	}
+	else if(annee === 2015) {
+		indice = 3;
+	}
+	else {
+		indice = 4;
+	}
+	$.get("data/data-instagram.json", function(data) {
+		if(annee != 2012) {
+			var nbInstaPrecedents = data[indice-1].nombre;
+			var array2 = nbInstaPrecedents.toString().split("").map(function(t){return parseInt(t)});
+		}
+
+		var nbInsta = data[indice].nombre;
+		var array = nbInsta.toString().split("").map(function(t){return parseInt(t)});
+
+
+		var label = "insta";
+		
+		// données de l'année précédente
+		if(annee != 2012) {
+			if((array2.length)!=5) {
+				var e = 5-array2.length;
+				for(var i=0; i<e; i++) {
+					array2.unshift(-1);
+				}
+			}
+
+			replaceAllDigitsSocial(array2, annee, label);
+		}
+		
+		var l = array.length;
+		var e = 5-l;
+		if((5-l) != 0) {
+			for(var i=0; i<e; i++) {
+				array.unshift(-1);
+			}
+		}
+
+		setTimeout(function(){ $('#'+annee+'insta0')['animateInstagram'+annee+0]('flipInX', array, annee, label); }, 1000);
+        setTimeout(function(){ $('#'+annee+'insta1')['animateInstagram'+annee+1]('flipInX', array, annee, label); }, 1100);
+        setTimeout(function(){ $('#'+annee+'insta2')['animateInstagram'+annee+2]('flipInX', array, annee, label); }, 1200);
+        setTimeout(function(){ $('#'+annee+'insta3')['animateInstagram'+annee+3]('flipInX', array, annee, label);}, 1300);
+        setTimeout(function(){ $('#'+annee+'insta4')['animateInstagram'+annee+4]('flipInX', array, annee, label);}, 1400);
+
+       	$('.'+annee+'insta').removeClass('animated flipInX')
 	});
 }
