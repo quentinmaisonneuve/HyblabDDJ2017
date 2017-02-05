4c92e3a5ef9cdfb82af4365bd511e7baa4930d02
e134578f <landry.descamps@etu.univ-nantes.fr>
2017-02-05 20:58:24 +0100

    Création du fichier pour data3

---
+++ le_crabe_fantome/public/Hyblab_Litoral/js/Script_data3.js
@@ -0,0 +1,11 @@
+	'use strict';
+
+$(".ScientifiqueJ3-1").fadeIn("slow");
+$(".ScientifiqueJ3-2").fadeOut("slow");
+$(".ScientifiqueJ3-3").fadeOut("slow");
+$(".ScientifiqueJ3-4").fadeOut("slow");
+
+$(".DataJ3-1").fadeOut("slow");
+$(".DataJ3-2").fadeOut("slow");
+$(".DataJ3-3").fadeOut("slow");
+$(".DataJ3-4").fadeOut("slow");
