c7b2627cd88634945371668bcb67da286dc6fbc5
Corentin MARIE <corentin.marie@etu.univ-nantes.fr>
2017-02-05 14:19:38 +0100

    [ADD] last slide + right font

--- ouest_france/public/js/responsive-text.js
+++ ouest_france/public/js/responsive-text.js
@@ -5,5 +5,6 @@ $("#text").fitText(2.3);
 $("#commencer").fitText();
 $("#center h2").fitText(0.25);
 $("#center h5").fitText(1.5);
-$("#paragraphe_encadre1").fitText(0.6);
+$("#paragraphe_encadre1").fitText(0.4);
 $("#number").fitText(0.2);
+$("#conclu1, #conclu2").fitText(4);
