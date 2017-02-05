961fbf4a47193abc59f0e75ad109aabeadea63cd
Corentin MARIE <corentin.marie@etu.univ-nantes.fr>
2017-02-05 13:42:11 +0100

    [ADD] responsive text

---
+++ ouest_france/public/js/responsive-text.js
@@ -0,0 +1,9 @@
+'use-strict'
+
+$("#enroute").fitText();
+$("#text").fitText(2.3);
+$("#commencer").fitText();
+$("#center h2").fitText(0.25);
+$("#center h5").fitText(1.5);
+$("#paragraphe_encadre1").fitText(0.6);
+$("#number").fitText(0.2);
