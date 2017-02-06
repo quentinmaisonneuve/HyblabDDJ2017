70f1201028db562c44b86c77a2cb14f72e41de11
Tanguy <tanguy.le-berre@etu.univ-nantes.fr>
2017-02-06 21:06:37 +0100

    [D3/CSS] histogramme dataviz 4

--- le_crabe_fantome/public/Hyblab_Litoral/js/graph_habitat.js
+++ le_crabe_fantome/public/Hyblab_Litoral/js/graph_habitat.js
@@ -1,10 +1,13 @@
 var svgContainer = d3.select(".chart_habitat");
 var anneContainer=d3.select(".list_annee");
-var height=svgContainer.node().getBoundingClientRect().height;
-var width=svgContainer.node().getBoundingClientRect().width;
+var width = parseInt(d3.select("#graphHabitat").style("width"));
+var height = parseInt(d3.select("#graphHabitat").style("height"));
 
+console.log(width);
 
 $.getJSON("data/habitat.json",function(dataJson) {
+    svgContainer.attr('height', height*0.8)
+      .attr('width', width*0.9);
 
     var buttonData=[];
     var data=[];
@@ -113,58 +116,4 @@ $.getJSON("data/habitat.json",function(dataJson) {
     })
 
 
-});
-
-
-
-
-
-var svg = d3.select("#histogramme"),
-    margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = +svg.attr("width") - margin.left - margin.right,
-    height = +svg.attr("height") - margin.top - margin.bottom;
-
-var x = d3.scale.ordinal()
-    .rangeRoundBands([0, width], .1);
-y = d3.scale.linear().rangeRound([height, 0]);
-
-var g = svg.append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-d3.tsv("data/data.tsv", function(d) {
-    d.frequency = +d.frequency;
-    return d;
-}, function(error, data) {
-    if (error) throw error;
-
-    x.domain(data.map(function(d) { return d.letter; }));
-    y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
-
-    g.append("g")
-        .attr("class", "axis axis--x")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.svg.axis()
-            .scale(x)
-            .orient("bottom"));
-
-    g.append("g")
-        .attr("class", "axis axis--y")
-        .call(d3.svg.axis()
-            .scale(y)
-            .orient("left").ticks(10, "%"))
-        .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", "0.71em")
-        .attr("text-anchor", "end")
-        .text("Taux");
-
-    g.selectAll(".bar")
-        .data(data)
-        .enter().append("rect")
-        .attr("class", "bar")
-        .attr("x", function(d) { return x(d.letter); })
-        .attr("y", function(d) { return y(d.frequency); })
-        .attr("width", x.rangeBand())
-        .attr("height", function(d) { return height - y(d.frequency); });
 });
