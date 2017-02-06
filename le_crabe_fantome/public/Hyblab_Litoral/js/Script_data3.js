beef36cc9aa48cc3d86e667972b988c8dba920b3
e134578f <landry.descamps@etu.univ-nantes.fr>
2017-02-06 22:54:42 +0100

    Coloration data3

--- le_crabe_fantome/public/Hyblab_Litoral/js/Script_data3.js
+++ le_crabe_fantome/public/Hyblab_Litoral/js/Script_data3.js
@@ -10,6 +10,12 @@ $("#Macération").click(function(e){
 	$(".DataJ3-2").fadeOut("slow");
 	$(".DataJ3-3").fadeOut("slow");
 	$(".DataJ3-4").fadeOut("slow");
+
+	$("#Macération").css("background-color", "#faf7f5");
+	$("#Ultrasons").css("background-color", "#6a9dae");
+	$("#Enzymes").css("background-color", "#6a9dae");
+	$("#Ultrasons_Enzymes").css("background-color", "#6a9dae");
+
 });
 
 $("#Ultrasons").click(function(e){
@@ -22,6 +28,11 @@ $("#Ultrasons").click(function(e){
 	$(".DataJ3-2").fadeIn("slow");
 	$(".DataJ3-3").fadeOut("slow");
 	$(".DataJ3-4").fadeOut("slow");
+
+	$("#Macération").css("background-color", "#6a9dae");
+	$("#Ultrasons").css("background-color", "#faf7f5");
+	$("#Enzymes").css("background-color", "#6a9dae");
+	$("#Ultrasons_Enzymes").css("background-color", "#6a9dae");
 });
 
 $("#Enzymes").click(function(e){
@@ -34,6 +45,11 @@ $("#Enzymes").click(function(e){
 	$(".DataJ3-2").fadeOut("slow");
 	$(".DataJ3-3").fadeIn("slow");
 	$(".DataJ3-4").fadeOut("slow");
+
+	$("#Macération").css("background-color", "#6a9dae");
+	$("#Ultrasons").css("background-color", "#6a9dae");
+	$("#Enzymes").css("background-color", "#faf7f5");
+	$("#Ultrasons_Enzymes").css("background-color", "#6a9dae");
 });
 
 $("#Ultrasons_Enzymes").click(function(e){
@@ -46,4 +62,9 @@ $("#Ultrasons_Enzymes").click(function(e){
 	$(".DataJ3-2").fadeOut("slow");
 	$(".DataJ3-3").fadeOut("slow");
 	$(".DataJ3-4").fadeIn("slow");
+
+	$("#Macération").css("background-color", "#6a9dae");
+	$("#Ultrasons").css("background-color", "#6a9dae");
+	$("#Enzymes").css("background-color", "#6a9dae");
+	$("#Ultrasons_Enzymes").css("background-color", "#faf7f5");
 });
