8dcd0ea0889f34b22ec88adfeb199f9f197f76d7
Meriem EL QSIMI <meriem.qs@gmail.com>
2017-02-07 12:33:45 +0100

    fixed animations

--- double_mixte/public/js/animateBus.js
+++ double_mixte/public/js/animateBus.js
@@ -1,15 +1,17 @@
 
 
 function animateBus(nomID){$("#"+nomID).animate({
- left: $("#"+nomID).parent().width()/2  - $("#"+nomID).width() }, 5000);
+ left: $("#"+nomID).parent().width()  }, 5000);
 }
 
 
 
 function animateFirstBus(){$("#busVan11").animate({
- left: $("#busVan11").parent().width() - $("#busVan11").width()}, 10000);
-	 $(".moveDiv")
-        .mouseover(function () { $(this).pauseAnimation(); })
-        .mouseout(function () { $(this).resumeAnimation(); });
+ left: $("#busVan11").parent().width() }, 10000);
+}
+
+
+function animateLastBus(){$("#busVan16").animate({
+ left: $("#busVan16").parent().width()/2- $("#busVan16").width()}, 5000);
 }
 
